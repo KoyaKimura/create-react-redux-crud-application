4145ebe276f32534623f5c3197d29a48f296208a
koyaKimura <swim.s1.frees@gmail.com>
2020-04-20 00:12:02 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 //Reducerでも使うため定義してexportしておく
 export const READ_EVNETS = "READ_EVENTS"
+export const CREATE_EVNET = "CREATE_EVENT"
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1"
 const QUERYSTRING = "?token=token123"
@@ -8,4 +9,9 @@ const QUERYSTRING = "?token=token123"
 export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   dispatch({ type: READ_EVNETS, response })
+}
+
+export const postEvent = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({ type: CREATE_EVNET, response })
 }
